83fb4721f0846ebdf23144f145c728adac13cf8c
moonstal1506 <moonstal1506@gmail.com>
2022-09-05 23:32:09 +0900

    feat: 상품 페이지 조회

---
+++ server/src/main/java/com/example/gccoffee/controller/ProductController.java
@@ -0,0 +1,22 @@
+package com.example.gccoffee.controller;
+
+import com.example.gccoffee.service.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@AllArgsConstructor
+@Controller
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("/products")
+    public String productsPage(Model model) {
+        var products = productService.getAllProducts();
+        model.addAttribute("products", products);
+        return "product-list";
+    }
+}
