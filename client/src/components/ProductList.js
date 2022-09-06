f8231d4a06e3405beee004f4cb72cf17da422392
moonstal1506 <moonstal1506@gmail.com>
2022-09-06 11:43:04 +0900

    feat: 상품 주문

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -9,7 +9,7 @@ export function ProductList({ products = [], onAddClick }) {
       </h5>
       <ul className="list-group products">
         {products.map((v) => (
-          <li key={v.id} className="list-group-item d-flex mt-3">
+          <li key={v.productId} className="list-group-item d-flex mt-3">
             <Product {...v} onAddClick={onAddClick} />
           </li>
         ))}
