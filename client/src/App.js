caa050724baecd738d0230709d2eeb136ead53b5
moonstal1506 <moonstal1506@gmail.com>
2022-09-06 10:25:27 +0900

    feat: 상품목록 화면

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import React, { useState } from "react";
+import { ProductList } from "./components/ProductList";
+import { Summary } from "./components/Summary";
 
 function App() {
+  // 상태에 접근할 수 있는 변수명, 상태값 바꾸는 함수
+  const [products, setProducts] = useState([
+    {
+      id: "uuid-1",
+      productName: "콜롬비아커피1",
+      category: "커피빈",
+      price: 3000,
+    },
+    {
+      id: "uuid-2",
+      productName: "콜롬비아커피2",
+      category: "커피빈",
+      price: 3000,
+    },
+    {
+      id: "uuid-3",
+      productName: "콜롬비아커피3",
+      category: "커피빈",
+      price: 3000,
+    },
+  ]); //기본상태
+  const [items, setItems] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body className="container-fluid">
+      <div className="row justify-content-center m-4">
+        <h1 className="text-center">Grids & Circle</h1>
+      </div>
+      <div className="card">
+        <div className="row">
+          <div className="col-md-8 mt-4 d-flex flex-column align-items-start p-3 pt-0">
+            <ProductList products={products} />
+          </div>
+          <div className="col-md-4 summary p-4">
+            <Summary items={items} />
+          </div>
+        </div>
+      </div>
+    </body>
   );
 }
 
